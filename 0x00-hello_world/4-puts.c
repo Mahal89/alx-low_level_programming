09c7329905921cdb32139329ae168f1208145713
Mahal89 <mondelumkwana@yahoo.com>
2023-02-16 23:54:32 +0200

    code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+/**
+ *starting point
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-(return 0);
+  printf("\"Programming is like building a multilingual puzzle");
 }
