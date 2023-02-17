331b61db0270ac0f388f9530735921383aec2865
Mahal89 <mondelumkwana@yahoo.com>
2023-02-18 00:31:52 +0200

    3-alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio>
+/**
+ *main - entry point
+ *Description: learning SE
+ *Return: always 0
+*/
+int main(void)
+{
+char ch = 'a';
+char c = 'A';
+for(ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+for(c = 'A'; c <= 'Z'; c++)
+{
+putchar(c);
+}
+return (0);
+}
+  
+  
