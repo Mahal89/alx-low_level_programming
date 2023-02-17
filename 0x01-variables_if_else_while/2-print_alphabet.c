fe5a285ba8a5ddcbe59f918263afe2a0c3d6c1a4
Mahal89 <mondelumkwana@yahoo.com>
2023-02-17 23:57:50 +0200

    variables

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  *main - entry point
- *description:learning_SE
- *return: always 0
+ *Description:learning_SE
+ *Return: always 0
 */
 int main(void)
 {
-char ch = 'a';
-for (ch = 'a';ch <= 'z';ch++)
+char ch = 'a' ;
+for (ch = 'a' ;ch <= 'z' ;ch++)
 {
 putchar(ch);
 }
