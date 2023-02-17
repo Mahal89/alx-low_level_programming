ec190c5734d8bb46fe5759116a7650a9f271ad81
Mahal89 <mondelumkwana@yahoo.com>
2023-02-17 23:46:56 +0200

    variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *description:learning_SE
+ *return: always 0
+*/
+int main(void)
+{
+char ch = 'a';
+for (ch = 'a';ch <= 'z';ch++)
+{
+putchar(ch);
+}
+return (0);
+}
