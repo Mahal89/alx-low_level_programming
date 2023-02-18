572d2d8cbb356601b00157543a245017bc270729
Mahal89 <mondelumkwana@yahoo.com>
2023-02-18 04:39:59 +0200

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description: learning SE
+ *Return: always 0
+*/
+int main(void)
+{
+char ch = 'z';
+if (ch = 'z'; ch >= 'a'; ch--)
+putchar(ch);
+return (0);
+}
