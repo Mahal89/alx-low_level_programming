39304d1d2801375e654cae2ae4bc3183270bbc81
Mahal89 <mondelumkwana@yahoo.com>
2023-02-18 05:48:34 +0200

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description: learning SE
+ *Return: always 0
+*/
+int main(void)
+{
+int n = 0;
+for (n = 0; n < 10; n++)
+putchar((n % 10) + '0')
+return (0);
+}
+  
+
