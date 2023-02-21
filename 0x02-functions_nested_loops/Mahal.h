1bcf45b2d6c5b1b2f7f225315fc7f8d58591b8f8
Mahal89 <mondelumkwana@yahoo.com>
2023-02-22 01:14:15 +0200

    header

---
+++ 0x02-functions_nested_loops/Mahal.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+char print_alphabet(char ch);
+int main(void)
+{
+char ch = 'a';
+char = print_alphabet (ch);
+putchar(ch);
+return (0);
+}
+char print_alphabet (ch)
+{
+char result;
+result = (ch = 'a'; ch <= 'z'; ch++)
+return result;
+}
