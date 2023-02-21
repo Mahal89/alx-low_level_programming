570422d838f751e3dea7775f85ecc90d701e81b4
Mahal89 <mondelumkwana@yahoo.com>
2023-02-21 03:26:06 +0200

    combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description: combinations
+ *Return: always 0
+*/
+int main(void)
+{
+int a = 0
+for (a = 0; a < 10; a++)
+putchar(n + '0');
+putchar(',');
+putchar(' ');
+putchar('\n');
+return (0);
+}
