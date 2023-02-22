98a675c47e3cae12f1fdfcbaf0caf4e1394c3e52
Mahal89 <mondelumkwana@yahoo.com>
2023-02-22 16:08:10 +0200

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
-#include "Mahal.h"
-/**
- *main - entry point
- *Description: loops
- *Return: always 0
-*/
-int main(void)
-{
+#include "main.h"
+
 void print_alphabet(void);
-char n;
-for (n = 'a'; n <= 'z'; n++)
-result = n;
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+char result = ch;
 return result;
 }
