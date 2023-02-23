a3fa92acf17b6746cd67cb69f59d998f1686f0a1
Mahal89 <mondelumkwana@yahoo.com>
2023-02-23 23:18:04 +0200

    more

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,9 +3,9 @@
 int main(void)
 {
 char n = 'a';
-_putchar("%c: %d\n", n, _isupper(n));
+putchar("%c: %d\n", n, _isupper(n));
 n = 'A';
-_putchar("%c: %d\n", n, _isupper(n))
+putchar("%c: %d\n", n, _isupper(n))
 return (0);
 }
     
