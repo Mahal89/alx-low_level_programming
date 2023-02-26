a116d3566e5822b98c86fd0c0e7e24b648c455f7
Mahal89 <mondelumkwana@yahoo.com>
2023-02-26 20:35:08 +0200

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *_islower - prints value when character is passed to islower
+ *_islower - prints value when character is passed to _islower
  *Return: 1 if its a lower case
  *Return: 0 if not a lower case
 */
 int _islower(int c)
 {
-char c;
+int c;
 c = 'b';
-_putchar("%c : %d", c, islower);
+_putchar("%c : %d", c, _islower);
 c = 'E';
-_putchar("%c : %d", c, islower);
+_putchar("%c : %d", c, _islower);
 }
