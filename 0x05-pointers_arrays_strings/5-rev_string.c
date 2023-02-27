d0e5192bc38a2eee66d8c92d8907177be13bfdf4
Mahal89 <mondelumkwana@yahoo.com>
2023-02-27 23:45:46 +0200

    rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *rev_string - reverses string
+ *@s: string reversed
+ *Return: 0
+*/
+void rev_string(char *s)
+{
+ char temp;
+ temp = *end;
+ *end = *begin;
+ *begin = temp;
+}
