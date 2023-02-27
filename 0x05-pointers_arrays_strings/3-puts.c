37718c96288e971563d2a51ba0852dea2f9d7fab
Mahal89 <mondelumkwana@yahoo.com>
2023-02-27 23:00:21 +0200

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*_puts - prints a string
+*@str: the string
+*Return: 0
+*/
+void _puts(char *str)
+{
+_puts("I do not fear computers. I fear the lack of them");
+_puts("\n");
+return (0);
+}
