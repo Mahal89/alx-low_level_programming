ef725758610e4a4d3790ed578ceeda6b950644a4
Mahal89 <mondelumkwana@yahoo.com>
2023-02-28 23:37:45 +0200

    rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,10 +10,13 @@ void rev_string(char *s)
 int length = strlen(s);
 int middle = length/2;
 char temp;
+int i;
 for (i = 0; i < middle; i++)
 {
 temp = s[i];
 s[i] = s[length - i - 1];
 s[length - i - 1] = temp;
+_putchar("%s", s);
+_putchar("\n");
 }
       
