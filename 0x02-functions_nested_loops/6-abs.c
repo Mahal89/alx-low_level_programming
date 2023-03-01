6fe4e8065bcf5eee0de3845074fc00f748657762
Mahal89 <mondelumkwana@yahoo.com>
2023-03-01 17:35:28 +0200

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_abs - prints absolute value
+ *Return: 0
+*/
+int _abs(int)
+{
+int x;
+if (x >= 0)
+{
+return (x);
+}
+else
+{
+return (-x);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,4 +8,6 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int print_last_digit(int);
+void print_to_98(int n);
+int _abs(int);
   
