473881343cc1dd40f66112a8e5cef3e36ea0eab0
Mahal89 <mondelumkwana@yahoo.com>
2023-03-02 22:58:12 +0200

    cat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,6 +10,7 @@ char *_strcat(char *dest, char *src);
 {
 char s[100];
 int k = 0, j = 0;
+char *str;
 while (dest[k] != '\0')
 {
 str[j] = dest[k];
@@ -19,8 +20,8 @@ j++;
 while (src[k] != '\0')
 {
 str[j] = src[k];
-k++
-j++
+k++;
+j++;
 }
 str[j] = '\0';
 return (dest);
