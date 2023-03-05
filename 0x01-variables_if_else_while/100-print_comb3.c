01ab7fa14dc5ab6875cce8d9e6e7ed492c188b1e
Mahal89 <mondelumkwana@yahoo.com>
2023-03-05 23:55:47 +0200

    comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: 0
+*/
+int main(void)
+{
+int k, j;
+for (k = 0; k < 9; k++)
+{
+for (j = j + 1; j < 10; j++)
+{
+putchar((k % 10) + '0');
+putchar((j % 10) + '0');
+if (k == 8 && j == 10)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+}
