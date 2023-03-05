94f3c693381dedbd4a486a597de2f7b09f05e2d3
Mahal89 <mondelumkwana@yahoo.com>
2023-03-05 10:42:02 +0200

    last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,6 +6,9 @@
 */
 int print_last_digit(int n)
 {
-_putchar((n % 10) + '0');
-return (n);
+int last_digit = n % 10;
+if (last_digit < 0)
+last_digit = last_digit * -1;
+_putchar(last_digit + '0');
+return (last_digit);
 }
