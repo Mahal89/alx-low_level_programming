e05097ab0616d49ab3eafa3ea9087e7774c4f4b5
Mahal89 <mondelumkwana@yahoo.com>
2023-03-05 22:29:11 +0200

    number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h>
+/**
+ *print_numbers - entry point
+ *Return: 0
+*/
+void print_numbers(void)
+{
+int k;
+for (k = 0; k <= 9; k++)
+_putchar(k + '0');
+_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 int _putchar(int char);
 int _isupper (int n);
 int _isdigit(int c);
+void print_numbers(void);
   
