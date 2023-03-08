94566837a78bc94e74749a310a7f8bcc57237926
Mahal89 <mondelumkwana@yahoo.com>
2023-03-09 01:25:43 +0200

    length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_strlen_recursion - prints length
+ *@s: character
+ *Return: 0
+*/
+int _strlen_recursion(char *s)
+{
+int k;
+if (*s)
+{
+_strlen_recursion(k = 0; s[k] != '\0', k++)
+_putchar("%d", k);
+}
+}
+      
