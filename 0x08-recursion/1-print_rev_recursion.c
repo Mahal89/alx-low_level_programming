7f65577ab8c78abb7ae18f44e63dfd9bae8e12f1
Mahal89 <mondelumkwana@yahoo.com>
2023-03-09 00:45:00 +0200

    rev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_print_rev_recursion - prints string
+ *@s: character
+ *Return: 0
+*/
+void _print_rev_recursion(char *s)
+{
+if (*s)
+{
+strrev(*s);
+_print_rev_recursion(s + 1);
+}
+  
