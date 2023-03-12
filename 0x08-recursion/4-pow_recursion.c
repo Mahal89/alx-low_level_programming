2e8e2a8e66422e28b65752a3d80cb32f9ddec0b1
Mahal89 <mondelumkwana@yahoo.com>
2023-03-12 16:24:59 +0200

    pow

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_pow_recursion - entry point
+ *@x: first argument
+ *@y: second argument
+ *Return: 0
+*/
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+else
+return (x * _pow_recursion(x, y-1))
+}
