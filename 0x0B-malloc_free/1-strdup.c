df47264b9210bb8a26a9bcaea5f5ebaaf7215bbe
Mahal89 <mondelumkwana@yahoo.com>
2023-03-16 00:19:00 +0200

    1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,11 +10,11 @@ char *_strdup(char *str)
 {
 char *str2;
 unsigned int size, k;
-str2 = malloc(size *(sizeof(char));
-if (str2 == NULL || str2 > size)
+str2 = malloc(size * sizeof(char));
+if (str == NULL || str2 > size)
 return (NULL);
 for (k = 0; k < size; k++)
-str = str2[k];
-strdup(str2);
-return (0)
+str[k] = str2[k];
+return(str2);
+}
 
