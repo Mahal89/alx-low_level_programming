7d714212697ae32094ede22c6a1eb36290075126
Mahal89 <mondelumkwana@yahoo.com>
2023-03-17 12:47:46 +0200

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_sqrt_recursion - entry point
+ *@n: argument
+ *Return: return -1 if n has no square root
+*/
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+return (-1);
+else
+sqrt(n);
+return (0);
+}
