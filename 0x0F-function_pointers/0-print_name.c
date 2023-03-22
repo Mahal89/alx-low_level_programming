3c815bed3dfcfc4bcb2e932bf317075cc302830f
Mahal89 <mondelumkwana@yahoo.com>
2023-03-22 21:29:43 +0200

    name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "function_pointer.h"
+/**
+ *print_name - entry point
+ *@name: 1st argument
+ *@f: 2nd argument
+ *Return: 0
+*/
+void print_name(char *name, void (*f)(char *))
+{
+f(char *);
+}
