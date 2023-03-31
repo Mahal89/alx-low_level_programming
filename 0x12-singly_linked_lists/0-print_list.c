33734a0547dd20ff0255e24ec943ac2bb8d2f4eb
Mahal89 <mondelumkwana@yahoo.com>
2023-03-31 22:24:17 +0200

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,12 +11,12 @@ size_t print_list(const list_t *h)
 size_t count = 0;
 while (h != NULL)
 {
-printf("%d", h->len, h->str);
+printf("[%d] %s", h->len, h->str);
 h = h->next;
 count++
 }
 if (h->str == NULL)
 {
-printf("(nil)");
+printf("[0] (nil)");
 }
 }
