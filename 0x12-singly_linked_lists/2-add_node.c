a6ac67fcfa6e9b00dabbc2727599139cb0392377
Mahal89 <mondelumkwana@yahoo.com>
2023-03-31 20:44:29 +0200

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,7 +13,9 @@ list_t *add_node(list_t **head, const char *str)
 {
 list_t *newnode = malloc(sizeof(list_t));
 if (newnode == NULL)
+{
 return (NULL);
+}
 newnode->len = strlen(str);
 newnode->str = strdup(str);
 newnode->next = *head;
