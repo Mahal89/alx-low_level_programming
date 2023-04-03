6377b79ffc41532608aa1ee09f6f300fdaca5eff
Mahal89 <mondelumkwana@yahoo.com>
2023-04-04 00:39:20 +0200

    new dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,18 +7,14 @@
  *@owner: dog owner
  *Description: describe dog
 */
-
 typedef struct dog
 {
 char *name;
 float age;
 char *owner;
 } dog_t;
-
 /* prototypes */
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-
 #endif
