aa46bfea47df19799350a901f302641e579aa3de
Mahal89 <mondelumkwana@yahoo.com>
2023-04-03 23:24:14 +0200

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,10 +13,16 @@ if (d == NULL)
 {
 return;
 }
+if (d->name == NULL)
+printf("Name: (nil)\n");
 else
-{
-printf("Name: %s\n", d->name ? d->name : "(nil)");
-printf("Age: %f\n", d->age ? d->age : "(nil)");
-printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
-}
+printf("Name: %s\n", d->name);
+if (d->age <= 0)
+printf("Age: (nil)\n");
+else
+printf("Age: %f\n", d->age);
+if (d->owner == NULL)
+printf("Owner: (nil)\n");
+else
+printf("Owner: %s\n", d->owner);
 }
