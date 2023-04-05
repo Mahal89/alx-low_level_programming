56d52670ae0c1ef9ff0e4a10559098ceeb53f25f
Mahal89 <mondelumkwana@yahoo.com>
2023-04-05 21:05:50 +0200

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,8 +10,12 @@
 char *_strdup(char *str)
 {
 char *str2;
-str2 = malloc(sizeof(char) * (strlen(str) + 1));
 if (str == NULL)
+{
+return (NULL);
+}
+str2 = malloc(sizeof(char) * (strlen(str) + 1));
+if (str2 == NULL)
 {
 return (NULL);
 }
