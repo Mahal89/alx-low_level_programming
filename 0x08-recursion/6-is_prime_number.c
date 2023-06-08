6d174ed53a003b79e6f9e70bdff7d7edace76c34
monde <mondelumkwana@yahoo.com>
2023-06-08 05:46:09 +0200

    task 7

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *is_prime_number - function
+ *@n: argument
+ *Return: 1 if prime number,otherwise 0
+*/
+int is_prime_number(int n)
+{
+int i;
+n = n / i;
+if (n < 2 ||  n % 2 = 0 || (i != 1 && i != n))
+{
+return (0);
+}
+else
+{
+return (1);
+}
+}
